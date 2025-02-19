7ab1eef5b35e5c913303333936065c6e2a6e0df7
Ersin Güneş <ersingunes28@gmail.com>
2025-02-19 21:32:39 +0300

    ölüm animasyonu ve ses denedim ama pek beğenmedim biraz daha olgunlaşması lazım projenin. Şuana kadar karakterin spawn noktası var ve her öldüğünde belirlediğimiz noktada başlıyor. Ölüm için de engellere çarpma ya da şimdilik belirlenen belli bir eksende düşüş olduğunda yeniden doğuyor. Kısa bir parkur yaptım. Standart sorunlarımızdan karakter zıpladığında japon yapıştırıcısı gibi yapışıyor olması onu çözecem ve zıplama kısmı biraz yavaş kalıyor update den çekecem onu fixed e yazabilirim belki biraz daha hızlı olur. Şimdilik bu kadar

--- Assets/Scripts/PlayerDeath.cs
+++ Assets/Scripts/PlayerDeath.cs
@@ -6,28 +6,34 @@ public class PlayerDeath : MonoBehaviour
 {
     public Transform spawnPoint; // Spawn noktasý
     public float deathYLevel = -10f; // Ölme seviyesi
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); // Rigidbody bileþenini al
+    }
 
     void Update()
     {
         // Eðer karakter belirlenen Y seviyesinin altýna düþerse
         if (transform.position.y < deathYLevel)
         {
-            Respawn(); // Respawn çaðýr
+            Respawn(); // Spawn noktasýna ýþýnla
         }
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        // Eðer çarptýðý obje "Trap" (veya baþka bir ölümcül obje) tagine sahipse
+        // Eðer çarptýðý obje "Trap" (engel) tagine sahipse
         if (other.CompareTag("Trap"))
         {
-            Respawn(); // Yeniden doðma fonksiyonunu çaðýr
+            Respawn(); // Spawn noktasýna ýþýnla
         }
     }
 
     void Respawn()
     {
         transform.position = spawnPoint.position; // Spawn noktasýna ýþýnla
-        GetComponent<Rigidbody2D>().velocity = Vector2.zero; // Hýzý sýfýrla (Yere düþme devam etmesin)
+        rb.velocity = Vector2.zero; // Hareketi sýfýrla (takýlmalarý engelle)
     }
 }
